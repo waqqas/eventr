9b6edf967334ac484b098298a6bdffd3f4043dc2
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2020-04-04 22:37:39 +0500

    WIP: Add UDP Receiver class and example app

---
+++ examples/udp_receiver1.cpp
@@ -0,0 +1,30 @@
+#include "udp_receiver.h"
+
+#include <iostream>
+
+using receiver_type = Eventr::udp_receiver<2048>;
+
+void on_receive(receiver_type &receiver, receiver_type::buffer_type buffer, size_t size)
+{
+  static int  count = 5;
+  std::string data(buffer.data(), size);
+  std::cout << "read : " << data << std::endl;
+  if (count-- == 0)
+  {
+    receiver.stop();
+  }
+}
+
+int main(void)
+{
+  Eventr::io_handler io(10);
+  receiver_type      receiver(io);
+
+  receiver.set_cb(
+      std::bind(on_receive, std::ref(receiver), std::placeholders::_1, std::placeholders::_2));
+  receiver.start();
+
+  io.run();
+
+  return 0;
+}
