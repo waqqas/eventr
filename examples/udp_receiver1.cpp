8a547e387619bc353eb7fbfe658990ea99f246e3
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2020-04-06 12:27:18 +0500

    Added function to UDP echo

--- examples/udp_receiver1.cpp
+++ examples/udp_receiver1.cpp
@@ -4,11 +4,15 @@
 
 using receiver_type = Eventr::udp_receiver<2048>;
 
-void on_receive(receiver_type &receiver, receiver_type::buffer_type buffer, size_t size)
+void on_receive(receiver_type &receiver, const receiver_type::buffer_type buffer, const size_t &size,
+                const sockaddr_in &remote_addr)
 {
   static int  count = 5;
   std::string data(buffer.data(), size);
   std::cout << "read : " << data << std::endl;
+
+  receiver.send(buffer, size, remote_addr);
+
   if (count-- == 0)
   {
     receiver.stop();
@@ -20,8 +24,8 @@ int main(void)
   Eventr::io_handler io(10);
   receiver_type      receiver(io);
 
-  receiver.set_cb(
-      std::bind(on_receive, std::ref(receiver), std::placeholders::_1, std::placeholders::_2));
+  receiver.set_cb(std::bind(on_receive, std::ref(receiver), std::placeholders::_1,
+                            std::placeholders::_2, std::placeholders::_3));
   receiver.bind("0.0.0.0", 5000);
   receiver.start();
 
